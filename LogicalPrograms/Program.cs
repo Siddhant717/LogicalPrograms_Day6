fa7e5676026d611937dd35e81def7771aa71115f
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 16:03:57 +0530

    FibonacciSeries solution added

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -7,6 +7,22 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Program");
+
+            int n1 = 0, n2 = 1, n3, i, number;
+
+            Console.WriteLine("Enter the number of elements");
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(n1 + " " + n2 + " ");
+
+            for (i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
             
         }   
     }
